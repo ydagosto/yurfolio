2a15b383afd5ab03f3a45eb1e8df95a57ba66f24
Yuri D'Agosto <ydagosto@linkedin.com>
2023-07-22 17:21:06 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="page-body">
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={HomePage} />
+          <Route path="/hola" Component={HomePage} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <p>Hello World</p>;
+};
+
+export default HomePage;
