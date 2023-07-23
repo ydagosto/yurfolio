207a996c191e7df2ec699a29cd75d0e8be550c91
Yuri D'Agosto <ydagosto@linkedin.com>
2023-07-23 10:25:55 -0700

    adding resume timeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.scss";
-import NavBar from "./components/NavBar";
+import "./App.css";
 import HomePage from "./pages/HomePage";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <Router>
       <div id="page-body">
-        <NavBar />
         <Routes>
-          <Route path="/" Component={HomePage} />
-          <Route path="/hola" Component={HomePage} />
+          <Route path="/" Component={Resume} />
+          <Route path="/resume" Component={Resume} />
         </Routes>
       </div>
     </Router>
