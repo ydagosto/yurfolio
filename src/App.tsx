e95f9fa7eeaa3eb89a8c4b1b5a97a0e729bd4675
Yuri D'Agosto <ydagosto@linkedin.com>
2023-09-16 09:04:19 -0700

    addiing home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Resume from "./pages/Resume";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Router>
       <div id="page-body">
         <Routes>
-          <Route path="/" Component={Resume} />
+          <Route path="/" Component={HomePage} />
           <Route path="/resume" Component={Resume} />
         </Routes>
       </div>
