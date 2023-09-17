12744b7d26acbccf7e1a439e8bb18ca26211001f
Yuri D'Agosto <ydagosto@linkedin.com>
2023-09-17 14:38:06 -0700

    creating a blob

--- src/components/Blob/index.tsx
+++ src/components/Blob/index.tsx
@@ -1,5 +1,21 @@
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+
 const Blob = () => {
-  return <div></div>;
+  // This reference will give us direct access to the mesh
+  const meshRef: any = useRef();
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta;
+    meshRef.current.rotation.y += delta;
+  });
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh ref={meshRef}>
+      <icosahedronGeometry args={[3, 0]} />
+      <meshLambertMaterial color={"gray"} />
+    </mesh>
+  );
 };
 
 export default Blob;
